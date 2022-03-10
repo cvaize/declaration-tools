90d181f29050f7eab5eb95d784f3c08ef995debf
cvaize <cvaize@gmail.com>
2022-03-10 14:45:18 +0500

    Функция генерации схемы почти закончена, осталось обработать undefined

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { get, set } from 'lodash';
+
 // Строковое представление типов данных. Не поддерживается bigint, date, symbol, function и другие специальные объекты
 export enum IdType {
 	number = 'number',
@@ -36,28 +38,42 @@ export const identifyType = (data: any): IdType => {
 	return result;
 };
 
-type SchemaItem = {
-	children: string[];
-};
-
 type Schema = {
-	[key: string]: SchemaItem;
+	[key: string]: Schema;
 };
 
 type SchemaResponse = {
 	schema: Schema;
 };
 
-const processingSchema = (data: any, schemaResponse: SchemaResponse): SchemaResponse => {
+const processingSchema = (data: any, schemaResponse: SchemaResponse, key: string): SchemaResponse => {
 	const type = identifyType(data);
-	if (!schemaResponse.schema[type]) {
-		schemaResponse.schema[type] = { children: [] };
+	const typeKey = `${key}.${type}`;
+
+	if (!get(schemaResponse, key)) {
+		set(schemaResponse, key, {});
+	}
+
+	if (!get(schemaResponse, typeKey)) {
+		set(schemaResponse, typeKey, {});
+	}
+
+	if (type === IdType.array) {
+		for (let i = 0; i < data.length; i += 1) {
+			processingSchema(data[i], schemaResponse, `${key}.array`);
+		}
+	} else if (type === IdType.object) {
+		const objectKeys = Object.keys(data);
+
+		for (let i = 0; i < objectKeys.length; i += 1) {
+			processingSchema(data[objectKeys[i]], schemaResponse, `${key}.object.${objectKeys[i]}`);
+		}
 	}
 
 	return schemaResponse;
 };
 
-export const makeSchema = (data: any) => processingSchema(data, { schema: {} });
+export const makeSchema = (data: any) => processingSchema(data, { schema: {} }, 'schema');
 
 export default {
 	identifyType,
